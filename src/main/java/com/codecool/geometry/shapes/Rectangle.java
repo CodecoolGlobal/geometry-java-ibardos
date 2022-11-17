f4bf8cb41d4a1af605a0c3f55d3fc08f19843360
Bárdos István <bardos.istvan@icloud.com>
2022-11-17 15:56:36 +0100

    Update Rectangle.java
    - Add necessary fields.
    - Implement constructors.
    - Override area and perimeter methods.
    - Override toString().

--- src/main/java/com/codecool/geometry/shapes/Rectangle.java
+++ src/main/java/com/codecool/geometry/shapes/Rectangle.java
@@ -1,14 +1,37 @@
 package com.codecool.geometry.shapes;
 
 public class Rectangle extends Shape {
+    private final float a;
+    private final float b;
+
+    public Rectangle(float a, float b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public float getA() {
+        return a;
+    }
+
+    public float getB() {
+        return b;
+    }
 
     @Override
     public double calculateArea() {
-        return 0;
+        return a * b;
     }
 
     @Override
     public double calculatePerimeter() {
-        return 0;
+        return 2 * a + 2 * b;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "a=" + a +
+                ", b=" + b +
+                '}';
     }
 }
