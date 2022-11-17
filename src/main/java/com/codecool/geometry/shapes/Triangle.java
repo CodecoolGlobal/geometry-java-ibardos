4ddef052a6ffb7b4755637a3a5c81062d3daeb3c
Bárdos István <bardos.istvan@icloud.com>
2022-11-17 15:57:16 +0100

    Update Triangle.java
    - Add necessary fields.
    - Implement constructors.
    - Override area and perimeter methods.
    - Override toString().

--- src/main/java/com/codecool/geometry/shapes/Triangle.java
+++ src/main/java/com/codecool/geometry/shapes/Triangle.java
@@ -1,14 +1,54 @@
 package com.codecool.geometry.shapes;
 
 public class Triangle extends Shape {
+    // Sides
+    private final float a;
+    private final float b;
+    private final float c;
+
+    // Semi-perimeter, for Area calculation with Heron's formula
+    private final double s;
+
+    public Triangle(float a, float b, float c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.s = (a + b + c) / 2;
+    }
+
+    public float getA() {
+        return a;
+    }
+
+    public float getB() {
+        return b;
+    }
+
+    public float getC() {
+        return c;
+    }
+
+    public double getS() {
+        return s;
+    }
 
     @Override
     public double calculateArea() {
-        return 0;
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
     }
 
     @Override
     public double calculatePerimeter() {
-        return 0;
+        return a + b + c;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "a=" + a +
+                ", b=" + b +
+                ", c=" + c +
+                ", s=" + s +
+                '}';
     }
 }
