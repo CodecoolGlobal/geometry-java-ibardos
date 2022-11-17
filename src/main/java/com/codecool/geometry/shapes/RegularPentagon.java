d5ef327d40b0d77cff477b9a3b59d66a97a27da7
Bárdos István <bardos.istvan@icloud.com>
2022-11-17 15:56:45 +0100

    Update RegularPentagon.java
    - Add necessary fields.
    - Implement constructors.
    - Override area and perimeter methods.
    - Override toString().

--- src/main/java/com/codecool/geometry/shapes/RegularPentagon.java
+++ src/main/java/com/codecool/geometry/shapes/RegularPentagon.java
@@ -1,14 +1,30 @@
 package com.codecool.geometry.shapes;
 
 public class RegularPentagon extends Shape {
+    private final float a;
+
+    public RegularPentagon(float a) {
+        this.a = a;
+    }
+
+    public float getA() {
+        return a;
+    }
 
     @Override
     public double calculateArea() {
-        return 0;
+        return a * a * Math.sqrt(5 * (5 + 2 * Math.sqrt(5))) / 4;
     }
 
     @Override
     public double calculatePerimeter() {
-        return 0;
+        return 5 * a;
+    }
+
+    @Override
+    public String toString() {
+        return "RegularPentagon{" +
+                "a=" + a +
+                '}';
     }
 }
