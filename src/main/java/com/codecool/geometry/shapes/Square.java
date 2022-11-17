b98104153ed6a7c2b5425abd21153462cd86c1cb
Bárdos István <bardos.istvan@icloud.com>
2022-11-17 15:57:06 +0100

    Update Square.java
    - Add necessary fields.
    - Implement constructors.
    - Override area and perimeter methods.
    - Override toString().
    - Extend from Shape, as extending from Rectangle resulted redundant data storing.

--- src/main/java/com/codecool/geometry/shapes/Square.java
+++ src/main/java/com/codecool/geometry/shapes/Square.java
@@ -1,5 +1,30 @@
 package com.codecool.geometry.shapes;
 
-public class Square extends Rectangle {
-    
+public class Square extends Shape {
+    private final float a;
+
+    public Square(float a) {
+        this.a = a;
+    }
+
+    public float getA() {
+        return a;
+    }
+
+    @Override
+    public double calculateArea() {
+        return a * a;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 4 * a;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "a=" + a +
+                '}';
+    }
 }
