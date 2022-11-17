8f087de48b284e9fca6e124b5a41a50eb70d80eb
Bárdos István <bardos.istvan@icloud.com>
2022-11-17 15:54:51 +0100

    Update Circle.java
    - Add necessary fields.
    - Implement constructors.
    - Override area and perimeter methods.
    - Override toString().

--- src/main/java/com/codecool/geometry/shapes/Circle.java
+++ src/main/java/com/codecool/geometry/shapes/Circle.java
@@ -1,14 +1,33 @@
 package com.codecool.geometry.shapes;
 
+import static java.lang.Math.PI;
+
 public class Circle extends Shape {
+    // Radius
+    private final float r;
+
+    public Circle(int r) {
+        this.r = r;
+    }
+
+    public float getR() {
+        return r;
+    }
 
     @Override
     public double calculateArea() {
-        return 0;
+        return PI * r * r;
     }
 
     @Override
     public double calculatePerimeter() {
-        return 0;
+        return 2 * PI * r;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "r=" + r +
+                '}';
     }
 }
