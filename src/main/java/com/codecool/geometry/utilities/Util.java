c13ff58cd0d16cd3588ea62635a9e3667fb12ec0
Bárdos István <bardos.istvan@icloud.com>
2022-11-23 23:06:50 +0100

    Create static Console object for input handling

--- src/main/java/com/codecool/geometry/utilities/Util.java
+++ src/main/java/com/codecool/geometry/utilities/Util.java
@@ -1,6 +1,12 @@
 package com.codecool.geometry.utilities;
+
+import java.io.Console;
 /**
  * A class to store reusable static helper methods, to be used throughout the whole application
  */
 public class Util {
+    /**
+     * A console object for handling user input via reading the console
+     */
+    public static final Console console = System.console();
 }
