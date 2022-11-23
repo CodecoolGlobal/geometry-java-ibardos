7c55fdb304b419c00af3f78acb04235cc1f86cc6
Bárdos István <bardos.istvan@icloud.com>
2022-11-23 23:17:58 +0100

    Create dependencies and fields necessary for application to run

--- src/main/java/com/codecool/geometry/utilities/ConsoleApplication.java
+++ src/main/java/com/codecool/geometry/utilities/ConsoleApplication.java
@@ -1,6 +1,23 @@
 package com.codecool.geometry.utilities;
+
+import com.codecool.geometry.containers.ShapeCollection;
+
+import static com.codecool.geometry.utilities.Util.*;
+
 /**
  * This class is intended to contain the main logic of the application
  */
 public class ConsoleApplication {
+    // Dependencies
+    private final MainMenu mainMenu;
+
+    // Fields
+    private boolean isRunning = true;
+    private boolean inputWasWrong = false;
+    private int option = -1;
+
+    // Constructor
+    public ConsoleApplication() {
+        this.mainMenu = new MainMenu(new ShapeCollection());
+    }
 }
