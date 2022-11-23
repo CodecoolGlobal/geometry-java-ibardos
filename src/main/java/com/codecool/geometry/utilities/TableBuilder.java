0ca68975be3a050660d58d201c36c5e6c4ef990a
Bárdos István <bardos.istvan@icloud.com>
2022-11-23 22:44:46 +0100

    Store necessary table column names

--- src/main/java/com/codecool/geometry/utilities/TableBuilder.java
+++ src/main/java/com/codecool/geometry/utilities/TableBuilder.java
@@ -5,4 +5,7 @@ package com.codecool.geometry.utilities;
  * A class containing logic and necessary helper methods for building a dynamic width table about Shape objects
  */
 public class TableBuilder {
+    // Fields
+    // Store column names
+    private final String[] columnNames = {"ID", "Class", "toString", "Perimeter", "Perimeter formula", "Area", "Area formula"};
 }
