7cf41f2f5dd55bc08316ec11720fdd706bc990ad
Bárdos István <bardos.istvan@icloud.com>
2022-11-23 23:07:28 +0100

    Implement pressEnterToContinue method
    Intended to be use during UI navigation.

--- src/main/java/com/codecool/geometry/utilities/Util.java
+++ src/main/java/com/codecool/geometry/utilities/Util.java
@@ -1,6 +1,8 @@
 package com.codecool.geometry.utilities;
 
 import java.io.Console;
+import java.io.IOException;
+
 /**
  * A class to store reusable static helper methods, to be used throughout the whole application
  */
@@ -9,4 +11,13 @@ public class Util {
      * A console object for handling user input via reading the console
      */
     public static final Console console = System.console();
+
+    /**
+     * Stops the execution and waits for user input, as an "enter" keypress
+     */
+    public static void pressEnterToContinue() throws IOException {
+        System.out.println("Press enter to continue.");
+        System.in.read();
+    }
+
 }
