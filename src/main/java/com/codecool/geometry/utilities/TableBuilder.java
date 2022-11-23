bd349bd6662bbeb318c83884c1a80c8c499287ee
Bárdos István <bardos.istvan@icloud.com>
2022-11-23 22:46:41 +0100

    Create buildTable method
    Build up the logic of dynamic width table creation

--- src/main/java/com/codecool/geometry/utilities/TableBuilder.java
+++ src/main/java/com/codecool/geometry/utilities/TableBuilder.java
@@ -1,5 +1,8 @@
 package com.codecool.geometry.utilities;
 
+import com.codecool.geometry.shapes.Shape;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -12,5 +15,33 @@ public class TableBuilder {
 
     // Store column names and their corresponding final lengths as key/value pairs
     private final HashMap<String, Integer> columnLengths = new HashMap<>();
+
+    // Methods
+    /**
+     * Stores logic to build a dynamic table of in memory stored shapes for console representation
+     * @param shapes list of Shape objects that are stored in memory
+     * @return a dynamic width table as String
+     */
+    public String buildTable(ArrayList<Shape> shapes) {
+        StringBuilder table = new StringBuilder();
+
+        // Determine the maximum lengths of table columns
+        setTableColumnLengths(shapes);
+
+        // Build a printable table: top line, header row, separator line, table data, bottom line
+        // Top line
+        table.append(createTopLine());
+
+        // Header row
+        table.append(createHeaderRow());
+
+        // Table data (separator line embedded)
+        table.append(createTableData(shapes));
+
+        // Bottom line
+        table.append(createBottomLine());
+
+        return table.toString();
+    }
 
 }
