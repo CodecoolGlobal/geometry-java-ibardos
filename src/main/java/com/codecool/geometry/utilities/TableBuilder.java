c18aa5f1f7095265dbfd0fe79bca7e766c10e533
Bárdos István <bardos.istvan@icloud.com>
2022-11-23 22:43:44 +0100

    Create TableBuilder.java

---
+++ src/main/java/com/codecool/geometry/utilities/TableBuilder.java
@@ -0,0 +1,8 @@
+package com.codecool.geometry.utilities;
+
+
+/**
+ * A class containing logic and necessary helper methods for building a dynamic width table about Shape objects
+ */
+public class TableBuilder {
+}
