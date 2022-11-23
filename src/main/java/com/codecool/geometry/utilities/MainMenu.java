45fd27070e173d5b832c798770cb8c3c17de31ea
Bárdos István <bardos.istvan@icloud.com>
2022-11-23 23:15:45 +0100

    Implement helper methods for Main menu
    - printFormulasOfShape
    - getParametersForShapeCreation
    - showMainMenu
    - shapeOptionsMenu

--- src/main/java/com/codecool/geometry/utilities/MainMenu.java
+++ src/main/java/com/codecool/geometry/utilities/MainMenu.java
@@ -147,6 +147,148 @@ public class MainMenu {
     }
 
 
+    // Private helper methods
+
+    /**
+     * Prints out to console the chosen Shape object's available formulas
+     * @param shapeChoiceForFormula shape choice as integer (from menu)
+     */
+    private void printFormulasOfShape(int shapeChoiceForFormula) {
+        switch (shapeChoiceForFormula) {
+            case 1:
+                System.out.println(AsciiArts.circle);
+                System.out.println("Area formula: " + FormulaProvider.getAreaFormulaForShape("Circle"));
+                System.out.println("Perimeter formula: " + FormulaProvider.getPerimeterFormulaForShape("Circle"));
+                System.out.println();
+                break;
+            case 2:
+                System.out.println(AsciiArts.rectangle);
+                System.out.println("Area formula: " + FormulaProvider.getAreaFormulaForShape("Rectangle"));
+                System.out.println("Perimeter formula: " + FormulaProvider.getPerimeterFormulaForShape("Rectangle"));
+                System.out.println();
+                break;
+            case 3:
+                System.out.println(AsciiArts.triangle);
+                System.out.println("Area formula: " + FormulaProvider.getAreaFormulaForShape("Triangle"));
+                System.out.println("Perimeter formula: " + FormulaProvider.getPerimeterFormulaForShape("Triangle"));
+                System.out.println();
+                break;
+            case 4:
+                System.out.println(AsciiArts.square);
+                System.out.println("Area formula: " + FormulaProvider.getAreaFormulaForShape("Square"));
+                System.out.println("Perimeter formula: " + FormulaProvider.getPerimeterFormulaForShape("Square"));
+                System.out.println();
+                break;
+            case 5:
+                System.out.println(AsciiArts.equilateralTriangle);
+                System.out.println("Area formula: " + FormulaProvider.getAreaFormulaForShape("EquilateralTriangle"));
+                System.out.println("Perimeter formula: " + FormulaProvider.getPerimeterFormulaForShape("EquilateralTriangle"));
+                System.out.println();
+                break;
+            case 6:
+                System.out.println(AsciiArts.regularPentagon);
+                System.out.println("Area formula: " + FormulaProvider.getAreaFormulaForShape("RegularPentagon"));
+                System.out.println("Perimeter formula: " + FormulaProvider.getPerimeterFormulaForShape("RegularPentagon"));
+                System.out.println();
+                break;
+        }
+    }
+
+    /**
+     * Asks the user to input the corresponding parameters for Shape object creation
+     * @param shapeChoice shape choice as integer (from menu)
+     * @return list of necessary parameters for corresponding Shape object creation
+     */
+    private ArrayList<Integer> getParametersForShapeCreation(int shapeChoice) {
+        ArrayList<Integer> parameterList = new ArrayList<>();
+
+        switch (shapeChoice) {
+            case 1: // Circle
+                System.out.println(AsciiArts.circle);
+                int rCircle = parseInt(console.readLine("Define radius: "));
+                parameterList.add(rCircle);
+                break;
+            case 2: // Rectangle
+                System.out.println(AsciiArts.rectangle);
+                int aRectangle = parseInt(console.readLine("Define side A: "));
+                int bRectangle = parseInt(console.readLine("Define side B: "));
+                parameterList.add(aRectangle);
+                parameterList.add(bRectangle);
+                break;
+            case 3: // Triangle
+                System.out.println(AsciiArts.triangle);
+                int aTriangle = parseInt(console.readLine("Define side A: "));
+                int bTriangle = parseInt(console.readLine("Define side B: "));
+                int cTriangle = parseInt(console.readLine("Define side C: "));
+                parameterList.add(aTriangle);
+                parameterList.add(bTriangle);
+                parameterList.add(cTriangle);
+                break;
+            case 4: // Square
+                System.out.println(AsciiArts.square);
+                int aSquare = parseInt(console.readLine("Define side A: "));
+                parameterList.add(aSquare);
+                break;
+            case 5: // EquilateralTriangle
+                System.out.println(AsciiArts.equilateralTriangle);
+                int aEquilateralTriangle = parseInt(console.readLine("Define side A: "));
+                parameterList.add(aEquilateralTriangle);
+                break;
+            case 6: // RegularPentagon
+                System.out.println(AsciiArts.regularPentagon);
+                int aRegularPentagon = parseInt(console.readLine("Define side A: "));
+                parameterList.add(aRegularPentagon);
+                break;
+        }
+
+        return parameterList;
+    }
+
+    /**
+     * Prints out the Main menu to console, and a "Wrong input!" message if the previous input was wrong
+     * @param inputWasWrong variable used as a switch to keep the state of the last user input (correct/wrong)
+     */
+    public void showMainMenu(boolean inputWasWrong) {
+        StringBuilder mainMenu = new StringBuilder()
+                .append(AsciiArts.mainMenu)
+                .append("1. Add new shape")
+                .append("\n" + "2. Show all shapes")
+                .append("\n" + "3. Show shape with the largest perimeter")
+                .append("\n" + "4. Show shape with the largest area")
+                .append("\n" + "5. Show formulas")
+                .append("\n" + "6. Credits")
+                .append("\n" + "0. Exit")
+                .append("\n");
+
+        if (inputWasWrong) {
+            mainMenu.append("\n").append("Wrong input! Try again.");
+        }
+
+        consoleClear();
+        System.out.println(mainMenu);
+    }
+
+    /**
+     * Returns a list of available Shape types for console representation
+     */
+    private String shapeOptionsMenu() {
+        StringBuilder shapeOptions = new StringBuilder()
+                .append("1. Circle")
+                .append("\n" + "2. Rectangle")
+                .append("\n" + "3. Triangle")
+                .append("\n" + "4. Square")
+                .append("\n" + "5. Equilateral triangle")
+                .append("\n" + "6. Regular pentagon")
+                .append("\n" + "0. Back to Main menu")
+                .append("\n");
+
+        if (inputWasWrong) {
+            shapeOptions.append("\n").append("Wrong input! Try again.");
+        }
+
+        return shapeOptions.toString();
+    }
+
     /**
      * Asks user input for shape choice, with the given text and ascii art as part of the UI
      * @param askInputText text as String, which will be used to ask the user with
