257c98c6c46a956b9e9c7d7a3f166ce1e102d378
Bárdos István <bardos.istvan@icloud.com>
2022-11-23 22:37:58 +0100

    Implement addShape method
    Algorithm able to create and add different Shape objects, based on different arguments.

--- src/main/java/com/codecool/geometry/containers/ShapeCollection.java
+++ src/main/java/com/codecool/geometry/containers/ShapeCollection.java
@@ -16,4 +16,37 @@ public class ShapeCollection {
         this.shapes = new ArrayList<>();
     }
 
+    /**
+     * Creates and adds a Shape object to the in memory collection
+     * @param shapeChoice integer value of Shape choice (from menu)
+     * @param parameters list of necessary parameters for corresponding Shape object creation
+     */
+    public void addShape(int shapeChoice, ArrayList<Integer> parameters) {
+        Shape shape = null;
+        int nextId = nextId();
+
+        switch (shapeChoice)
+        {
+            case 1:
+                shape = new Circle(parameters.get(0), nextId);
+                break;
+            case 2:
+                shape = new Rectangle(parameters.get(0), parameters.get(1), nextId);
+                break;
+            case 3:
+                shape = new Triangle(parameters.get(0), parameters.get(1), parameters.get(2), nextId);
+                break;
+            case 4:
+                shape = new Square(parameters.get(0), nextId);
+                break;
+            case 5:
+                shape = new EquilateralTriangle(parameters.get(0), nextId);
+                break;
+            case 6:
+                shape = new RegularPentagon(parameters.get(0), nextId);
+                break;
+        }
+
+        shapes.add(shape);
+    }
 }
