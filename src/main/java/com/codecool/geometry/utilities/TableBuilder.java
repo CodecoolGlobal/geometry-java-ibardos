57f9bd3f16ee875e521772d7dae4b0f72ca17ba5
Bárdos István <bardos.istvan@icloud.com>
2022-11-23 22:45:44 +0100

    Create a HashMap<String, Integer> to store column maximum lengths

--- src/main/java/com/codecool/geometry/utilities/TableBuilder.java
+++ src/main/java/com/codecool/geometry/utilities/TableBuilder.java
@@ -1,5 +1,6 @@
 package com.codecool.geometry.utilities;
 
+import java.util.HashMap;
 
 /**
  * A class containing logic and necessary helper methods for building a dynamic width table about Shape objects
@@ -8,4 +9,8 @@ public class TableBuilder {
     // Fields
     // Store column names
     private final String[] columnNames = {"ID", "Class", "toString", "Perimeter", "Perimeter formula", "Area", "Area formula"};
+
+    // Store column names and their corresponding final lengths as key/value pairs
+    private final HashMap<String, Integer> columnLengths = new HashMap<>();
+
 }
